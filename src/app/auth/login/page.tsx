d115d7da98e845773cd905ae26c02ddb92bf60b2
Snake <116700023+BleedingSnake@users.noreply.github.com>
2023-11-27 17:41:50 +0700

    Update page.tsx for login

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,7 +1,56 @@
+import React, { useState } from 'react';
+
 export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    // You can add your authentication logic here
+    
+    console.log('Username:', username);
+    console.log('Password:', password);
+
+    // You can redirect the user to another page after successful login
+    //  use -> history.push('/dashboard');
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Login Page
+      <h2 className="text-3xl font-bold mb-8">Login Page</h2>
+      <form onSubmit={handleLogin} className="flex flex-col items-center">
+        <label htmlFor="username" className="mb-2">
+          Username:
+        </label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="border p-2 mb-4"
+          required
+        />
+
+        <label htmlFor="password" className="mb-2">
+          Password:
+        </label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="border p-2 mb-8"
+          required
+        />
+
+        <button
+          type="submit"
+          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
+        >
+          Login
+        </button>
+      </form>
     </main>
-  )
+  );
 }
